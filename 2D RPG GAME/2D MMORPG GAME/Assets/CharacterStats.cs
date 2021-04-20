954579b8a828704acb2e2fd8c35d8c62a5a80b8c
wertiyou <ozgursakar44@gmail.com>
2021-04-20 20:16:15 +0300

    can sistemi eklendi
    Oyuna can sistemi eklendi henüz düşman olmadığı için j tuşuna basınca can yenileniyor h tuşuna basınca karakter hasar alıyor

---
+++ 2D RPG GAME/2D MMORPG GAME/Assets/CharacterStats.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class CharacterStats : MonoBehaviour
+{
+	//UI
+	public Image[] hearts;
+
+	//Stats
+	public int health = 5;
+	int maxHealth = 5;
+
+	public void Damage(int amount)
+	{
+		hearts[health - 1].enabled = false;
+		health -= amount;
+	}
+
+	public void Regen(int amount)
+	{
+		health += amount;
+
+		for(int i = 0; i < health; i++)
+		{
+			hearts[i].enabled = true;
+		}
+	}
+
+	private void Update()
+	{
+		if(health > maxHealth)
+		{
+			health = maxHealth;
+		}
+
+		if (Input.GetKeyDown(KeyCode.H))
+		{
+			if(health > 0)
+			{
+				Damage(1);
+			}
+		}
+
+		if (Input.GetKeyDown(KeyCode.J))
+		{
+			if(health < maxHealth)
+			{
+				Regen(1);
+			}
+		}
+	}
+}
